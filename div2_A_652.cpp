20c6d2e19cf8f320351e6b7d78465eac17a11566
Shivam <shiv.rawat594@gmail.com>
2020-07-09 17:57:50 +0530

    added a new file

---
+++ div2_A_652.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+#include <queue>
+#include <string>
+#include <cmath>
+#include <string>
+using namespace std;
+typedef long long ll;
+
+int main(){
+#ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("inputf.in", "r", stdin);
+    // for writing output to output.txt
+    freopen("outputf.in", "w", stdout);
+#endif
+int x;
+cin >> x;
+cout << x;
+    return 0;
+}
